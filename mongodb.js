d71095a5359b5496ca78c82e9ab0efc956ae9bf3
Anthony Moss <asmoss93@gmail.com>
2019-08-13 13:08:14 -0400

    adds example of insert many to insert multiple docs to users collection

--- mongodb.js
+++ mongodb.js
@@ -6,15 +6,38 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database!')
     } 
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Anthony',
-        age: 26
+    // db.collection('users').insertOne({
+    //     name: 'Anthony',
+    //     age: 26
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('users').insertMany([
+        {
+            name: 'Jen',
+            age: 28
+        },
+        {
+            name: 'Gunther',
+            age: 30
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents!')
+        }
+
+        console.log(result.ops)
     })
 })
