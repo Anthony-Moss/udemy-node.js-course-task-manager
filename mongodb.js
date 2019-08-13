bffd88a1669eff0fedfa201705337b6201d472b1
Anthony Moss <asmoss93@gmail.com>
2019-08-13 15:14:05 -0400

    removes insert calls

--- mongodb.js
+++ mongodb.js
@@ -9,10 +9,6 @@ const { MongoClient, ObjectID } = require ('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database!')
@@ -20,54 +16,5 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     _id: id,
-    //     name: 'Jose',
-    //     age: 23
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert user')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Jen',
-    //         age: 28
-    //     },
-    //     {
-    //         name: 'Gunther',
-    //         age: 30
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert documents!')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'do laundry',
-    //         completed: false
-    //     },
-    //     {
-    //         description: 'go to store',
-    //         completed: false
-    //     },
-    //     {
-    //         description: 'walk the dogs',
-    //         completed: true
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         console.log('Unable to insert documents!')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
+    
 })
