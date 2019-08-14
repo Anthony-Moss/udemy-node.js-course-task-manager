c46d4cbb8ac289372bbda10b376c89a7a0d9a8ad
Anthony Moss <asmoss93@gmail.com>
2019-08-14 13:56:37 -0400

    adds post call to /users and console logs reponse given and sends a placeholder string via res.send

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3002
 
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    res.send('testing!')
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}!`)
 })
