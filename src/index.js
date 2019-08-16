a1f62b32a5bc44eb465f4e58fae6a38f7eb69438
Anthony Moss <asmoss93@gmail.com>
2019-08-15 21:30:59 -0400

    adds patch request to tasks to update a task when given id has error handling

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ app.patch('/users/:id', async (req, res) => {
     if (!isValidOperation) {
         return res.status(404).send({ error: 'Invalid updates'})
     }
-    
+
     try  {
         const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, useFindAndModify: false })
         if (!user) {
@@ -93,6 +93,27 @@ app.get('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (e) {
         return res.status(404).send()
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        res.status(400).send({error: 'Invalid updates!'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, useFindAndModify: false })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
     }
 })
 
