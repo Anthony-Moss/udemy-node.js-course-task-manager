000aa173e4a2c0b13e874f629f96d28addfe0f76
Anthony Moss <asmoss93@gmail.com>
2019-08-15 23:12:42 -0400

    adds simple example of hashing password with bcrypt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require('./db/mongoose');
+const bcrypt = require('bcryptjs');
+
 const userRouter = require('./routes/users');
 const taskRouter = require('./routes/tasks');
 
@@ -10,7 +12,15 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+const myFunction = async () => {
+    const password = 'Waffles123!'
+    const hashedPassword = await bcrypt.hash(password, 8)
 
+    console.log(password)
+    console.log(hashedPassword)
+}
+
+myFunction()
 app.listen(port, () => {
     console.log(`Server is running on port ${port}!`)
 })
