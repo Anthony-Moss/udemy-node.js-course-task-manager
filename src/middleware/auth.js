774a92f30e7cbe5585e576d0a3d9becf00d6a467
Anthony Moss <asmoss93@gmail.com>
2019-08-16 22:01:36 -0400

    creates token variable to hold the token if user has one

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,17 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
 const auth = async (req, res, next) => {
-    console.log('auth middleware')
-    next()
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        if (token) {
+
+        } else {
+            res.status(401)
+        }
+    } catch (e) {
+        res.status(401).send({error: 'Please authenticate'})
+    }
 }
 
 module.exports = auth
