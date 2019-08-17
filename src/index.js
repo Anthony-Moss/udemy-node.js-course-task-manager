842e7d48820f81be6200ab04dcba66e44972a035
Anthony Moss <asmoss93@gmail.com>
2019-08-16 20:09:32 -0400

    adds example middleware to make get requests non accessable

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ const taskRouter = require('./routes/tasks');
 const app = express();
 const port = process.env.PORT || 3002
 
+app.use((req, res, next) => {
+    if (req.method === 'GET') {
+        res.status(400).send('Get requests are disabled')
+    } else {
+        next()
+    }
+})
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
