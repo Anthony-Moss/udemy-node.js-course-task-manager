3c89357dea388116b9b04ac23812fbde15b16417
Anthony Moss <asmoss93@gmail.com>
2019-08-16 20:18:31 -0400

    comments out examples

--- src/index.js
+++ src/index.js
@@ -16,11 +16,11 @@ const port = process.env.PORT || 3002
 //     }
 // })
 
-app.use((req, res, next) => {
-    if (req.method) {
-        res.status(503).send('Site is under mantienance please try back later')
-    }
-})
+// app.use((req, res, next) => {
+//     if (req.method) {
+//         res.status(503).send('Site is under maintenance please try back later')
+//     }
+// })
 
 app.use(express.json());
 app.use(userRouter);
