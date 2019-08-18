3a8e34c3dbd82d37138f4deebf560597986789c4
Anthony Moss <asmoss93@gmail.com>
2019-08-18 18:50:03 -0400

    get request for tasks now requires auth so users can only pull tasks by id if they created the task

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -4,7 +4,6 @@ const auth  = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/tasks', auth, async (req, res) => {
-    // const task = new Task(req.body)
     const task = new Task({
         ...req.body,
         owner: req.user._id
@@ -26,11 +25,11 @@ router.get('/tasks', async (req, res) => {
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: req.user._id})
         if (!task) {
             return res.status(404).send()
         }
