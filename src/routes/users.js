7c5c7cc36985fc09068d2919a9cea8db2e4f12cd
Anthony Moss <asmoss93@gmail.com>
2019-08-17 21:16:01 -0400

    updates delete request to need auth and new address is used so users can only delete own account

--- src/routes/users.js
+++ src/routes/users.js
@@ -22,7 +22,7 @@ router.post('/users/login', async (req, res) => {
     const user = await User.findByCredentials(req.body.email, req.body.password)
     try {
         const token = await user.generateAuthToken()
-        res.send({ user: user.getPublicProfile(), token })
+        res.send({ user, token })
     } catch (e) {
         res.status(400).send()
     }
@@ -54,19 +54,20 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
 
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+// router.get('/users/:id', async (req, res) => {
+//     const _id = req.params.id
+
+//     try {
+//         const user = await User.findById(_id)
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+//         res.send(user)
+//     } catch (e) {
+//         res.status(400).send(e)
+//     }
+// })
 
 router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
@@ -92,14 +93,10 @@ router.patch('/users/:id', async (req, res) => {
     }
 })
 
-router.delete('/users/:id', async (req, res)  => {
+router.delete('/users/me', auth, async (req, res)  => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send(e)
     }
