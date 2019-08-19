6beb32cd7e4b6832785c1ceaf60d88d1a95b1f2b
Anthony Moss <asmoss93@gmail.com>
2019-08-19 19:44:37 -0400

    sets up better error handling for /users/me/avatar, also removes old commeneted out unused code

--- src/routes/users.js
+++ src/routes/users.js
@@ -78,20 +78,6 @@ router.get('/users/me', auth, async (req, res) => {
 })
 
 
-// router.get('/users/:id', async (req, res) => {
-//     const _id = req.params.id
-
-//     try {
-//         const user = await User.findById(_id)
-//         if (!user) {
-//             return res.status(404).send()
-//         }
-//         res.send(user)
-//     } catch (e) {
-//         res.status(400).send(e)
-//     }
-// })
-
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
