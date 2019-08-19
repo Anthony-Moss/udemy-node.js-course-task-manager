63def170a794c2ab524a318e4dc3e01cce81eac9
Anthony Moss <asmoss93@gmail.com>
2019-08-19 17:49:10 -0400

    adds example of using multer to upload image

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ const taskRouter = require('./routes/tasks');
 const app = express();
 const port = process.env.PORT || 3002
 
-
+const multer = require('multer');
+const upload = multer({
+    dest: 'images'
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
 
 
 app.use(express.json());
