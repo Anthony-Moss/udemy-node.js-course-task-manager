5bd028a03787d0d011df15114af1e3c5b3388f0f
Anthony Moss <asmoss93@gmail.com>
2019-08-19 18:10:09 -0400

    adds example of how to use fileFilter to validate uploads

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ const upload = multer({
     dest: 'images',
     limits: {
         fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a PDF'))
+        }
+
+        cb(undefined, true)
     }
 })
 app.post('/upload', upload.single('upload'), (req, res) => {
