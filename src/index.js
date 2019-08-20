81257bd9edc1ccc471377e2dd7dc649d83f55189
Anthony Moss <asmoss93@gmail.com>
2019-08-20 18:57:58 -0400

    removes dev script hardcode in index.js moves to config/dev.env

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const userRouter = require('./routes/users');
 const taskRouter = require('./routes/tasks');
 
 const app = express();
-const port = process.env.PORT || 3002
+const port = process.env.PORT
 
 app.use(express.json());
 app.use(userRouter);
