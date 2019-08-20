02ec784db773caf6525e66b3c61d7ebc0f074d47
Anthony Moss <asmoss93@gmail.com>
2019-08-20 19:16:10 -0400

    moves JTW token and mongodb url to dev.env file

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,8 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
-const sendGridAPIKey = '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'
-
-sgMail.setApiKey(sendGridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
