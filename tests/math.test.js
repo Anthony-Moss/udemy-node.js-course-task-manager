e37b159ee483174ac6cfbbc5414ca28a07ef5386
Anthony Moss <asmoss93@gmail.com>
2019-08-22 19:50:20 -0400

    adds jest expect tobe method to test current example tip function

--- tests/math.test.js
+++ tests/math.test.js
@@ -3,9 +3,6 @@ const { calculateTip } = require('../src/math');
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
-
-    if (total !== 13) {
-        throw new Error(`Total tip should be 13, got ${total}`)
-    }
+    expect(total).toBe(13)
 })
 
