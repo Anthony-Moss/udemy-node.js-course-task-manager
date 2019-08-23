b6c98550fa1e4bda343afc3cf8f1287343dad393
Anthony Moss <asmoss93@gmail.com>
2019-08-22 20:27:24 -0400

    test now runs jest in watch mode adds method to test async functions

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math');
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math');
 
 
 test('Should calculate total with tip', () => {
@@ -19,4 +19,19 @@ test('Should convert 32 F to 0 C', () => {
 test("Should convert 0 C to 32 F", () => {
     const temp = celsiusToFahrenheit(0)
     expect(temp).toBe(32)
+})
+
+// test('Async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000)
+    
+// })
+
+test('Should add two numbers', (done) => {
+    add(2, 3).then((sum) => {
+        expect(sum).toBe(5)
+        done() 
+    })
 })
