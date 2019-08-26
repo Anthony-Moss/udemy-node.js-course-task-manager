131396419c7c97d0407148784dcdcef613728d08
Anthony Moss <asmoss93@gmail.com>
2019-08-26 17:34:55 -0400

    adds test data for a user and saves it to db in beforeEach so we can test functions that require a user, adds the function to test if a user can login with an exisiting account

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,8 +2,15 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/models/user');
 
+const userOne = {
+    name: 'Jotaro',
+    email: 'Jojo@goodgrief.com',
+    password: 'StarPlat!'
+}
+
 beforeEach(async () => {
     await User.deleteMany()
+    await new User(userOne).save()
 })
 
 test('Should signup a new user', async () => {
@@ -12,4 +19,8 @@ test('Should signup a new user', async () => {
         email: 'Anthony@me.com',
         password: 'MyPass123!'
     }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send(userOne).expect(200)
 })
