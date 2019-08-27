cd2dd642ba3e1649934a3d364e1cdab953571cca
Anthony Moss <asmoss93@gmail.com>
2019-08-26 21:16:57 -0400

    adds assertion to user login test that ensure that users token is saved correctly when logging in

--- src/models/user.js
+++ src/models/user.js
@@ -63,7 +63,6 @@ userSchema.virtual('tasks', {
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-
     const token = jwt.sign({ _id: user.id.toString() }, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({ token })
