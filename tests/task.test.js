3a8af9de53dd526af7cf8ddc16eb19b3c1cf6278
Anthony Moss <asmoss93@gmail.com>
2019-08-26 23:46:37 -0400

    adds test to ensure user can create task by expecting response to be a 201

--- tests/task.test.js
+++ tests/task.test.js
@@ -6,5 +6,12 @@ const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
 beforeEach(setupDatabase)
 
 test('Should create task for user', async () => {
+    await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'Test task'
+        })
+        .expect(201)
 
 })
