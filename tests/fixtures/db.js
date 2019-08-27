009f9444a69d58af558bbf428a8c8598c0841f96
Anthony Moss <asmoss93@gmail.com>
2019-08-26 23:58:21 -0400

    adds another user and some tasks to dummy test data

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = require('../../src/models/user');
+const Task = require('../../src/models/task');
+
 
 const userOneId = new mongoose.Types.ObjectId()
-
 const userOne = {
     _id: userOneId,
     name: 'Jotaro',
@@ -14,13 +15,62 @@ const userOne = {
     }]
 }
 
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+    _id: userTwoId,
+    name: 'Jonathan',
+    email: 'Jojo@theorigional.com',
+    password: 'HamonStrike@',
+    tokens: [{
+        token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
+    }]
+}
+
+
+
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Kill Dio',
+    completed: false,
+    owner: userOneId
+}
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Save mother',
+    completed: false,
+    owner: userTwoId
+}
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Kill Dio',
+    completed: false,
+    owner: userTwoId
+}
+
+const taskFour = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Learn to use Hamon',
+    completed: true,
+    owner: userOneId
+}
+
 const setupDatabase = async () => {
     await User.deleteMany()
+    await Task.deleteMany()
     await new User(userOne).save()
+    await new User(userTwo).save()
+    await new Task(taskOne).save()
+    await new Task(taskTwo).save()
+    await new Task(taskThree).save()
+    await new Task(taskFour).save()
 }
 
 module.exports = {
     userOne,
     userOneId,
+    userTwo,
+    userTwoId,
     setupDatabase
 }
